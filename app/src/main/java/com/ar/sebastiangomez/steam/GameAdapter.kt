700a9503ac2c559572fee641443fbb35a5fc705e
Sebastian Gomez <SSGOMEZ@trabajo.gob.ar>
2024-04-23 00:57:04 -0300

    Enviado de ID del juego al DetalleActivity

--- app/src/main/java/com/ar/sebastiangomez/steam/GameAdapter.kt
+++ app/src/main/java/com/ar/sebastiangomez/steam/GameAdapter.kt
@@ -1,16 +1,15 @@
 package com.ar.sebastiangomez.steam
 
+import android.content.Intent
 import android.graphics.Typeface
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 
-class GameAdapter(private val gamesList: List<Game>, private val onItemClick: (position: Int) -> Unit) : RecyclerView.Adapter<GameAdapter.GameViewHolder>() {
-
+class GameAdapter(private val gamesList: List<Game>, private val onItemClick: (position: Int, gameId: String) -> Unit) : RecyclerView.Adapter<GameAdapter.GameViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GameViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(android.R.layout.simple_list_item_1, parent, false)
         return GameViewHolder(view)
@@ -25,7 +24,14 @@ class GameAdapter(private val gamesList: List<Game>, private val onItemClick: (p
         holder.textView.setTypeface(null, Typeface.BOLD)
 
         holder.itemView.setOnClickListener {
-            onItemClick.invoke(position)
+            onItemClick.invoke(position, game.id) // Pasar el ID del juego al onItemClick
+            var selectID = game.id.toInt()
+            Log.d("ID POSICION:", game.id)
+            Log.d("SELECT ID:", selectID.toString())
+
+            val intent = Intent(holder.itemView.context, DetalleActivity::class.java)
+            intent.putExtra("ID", selectID)
+            holder.itemView.context.startActivity(intent)
         }
     }
 
