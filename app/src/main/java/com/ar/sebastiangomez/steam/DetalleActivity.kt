dbe6fea0bfa767c5d5dfc68aba38547a8bc0decb
Sebastian Gomez <ssgomez@trabajo.gob.ar>
2024-04-28 13:20:10 -0300

    Cambios en el objeto de GameDetail pc_requirement.

--- app/src/main/java/com/ar/sebastiangomez/steam/DetalleActivity.kt
+++ app/src/main/java/com/ar/sebastiangomez/steam/DetalleActivity.kt
@@ -38,6 +38,11 @@ class DetalleActivity : AppCompatActivity() {
     private lateinit var CardView : CardView
     private lateinit var headerImg : ImageView
     private lateinit var themeButton : ImageButton
+    private lateinit var textSO : TextView
+    private lateinit var textProcesador : TextView
+    private lateinit var textMemoria : TextView
+    private lateinit var textGraficos : TextView
+    private lateinit var textAlmacenamiento : TextView
     private val tag = "LOG-DETAIL"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -63,6 +68,11 @@ class DetalleActivity : AppCompatActivity() {
         headerImg = findViewById(R.id.imageDetalle)
         progressBar = findViewById(R.id.progressBarDetail)
         themeButton = findViewById(R.id.themeButton)
+        textSO = findViewById(R.id.textSO)
+        textProcesador = findViewById(R.id.textProcesador)
+        textMemoria = findViewById(R.id.textMemoria)
+        textGraficos = findViewById(R.id.textGraficos)
+        textAlmacenamiento = findViewById(R.id.textAlmacenamiento)
 
         val preferences = getSharedPreferences("ThemePrefs", Context.MODE_PRIVATE)
         val currentTheme = preferences.getString("theme", "light") // Obtén el tema actual
@@ -100,8 +110,13 @@ class DetalleActivity : AppCompatActivity() {
                         val gameDetail = gameDetailMap[gameId]?.data
                         if (gameDetail != null) {
                             // Verifica si las propiedades son nulas antes de usarlas
-                            Log.d(tag,"GAME DETAIL - ID: ${gameDetail.steam_appid}, Name: ${gameDetail.name}, Type: ${gameDetail.type}, Short description: ${gameDetail.short_description}")
-
+                            val pcRequirements = parsePcRequirements(gameDetail.pc_requirements.toString())
+                            Log.d(tag,"GAME DETAIL - ID: ${gameDetail.steam_appid}, Name: ${gameDetail.name}, Type: ${gameDetail.type}, Short description: ${gameDetail.short_description}, Pc requirements: $pcRequirements")
+                            textSO.text = pcRequirements?.minimum?.os ?: "N/A"
+                            textProcesador.text = pcRequirements?.minimum?.processor ?: "N/A"
+                            textMemoria.text = pcRequirements?.minimum?.memory ?: "N/A"
+                            textGraficos.text = pcRequirements?.minimum?.graphics ?: "N/A"
+                            textAlmacenamiento.text = pcRequirements?.minimum?.storage ?: "N/A"
                             TitleTxt.text = gameDetail.name
                             DescripcionTxt.text = gameDetail.short_description.replace(Regex("<br />|&quot;"), "")
                             // Cargar la imagen utilizando Glide
@@ -132,6 +147,73 @@ class DetalleActivity : AppCompatActivity() {
                 Log.e(tag, "ERROR: Failed to fetch game details: ${e.message}")
             }
         })
+    }
+
+    fun parsePcRequirements(pcRequirementsString: String): GameDetail.PcRequirements? {
+        // Elimina las etiquetas HTML y caracteres innecesarios
+        val cleanString = pcRequirementsString
+            .replace(Regex("<.*?>"), "") // Elimina etiquetas HTML
+            .replace("Minimum:", "") // Elimina el texto "Minimum:"
+            .replace("Recommended:", "") // Elimina el texto "Recommended:"
+            .replace(", recommended=", "") // Elimina el texto ", recommended="
+            .replace("\n", "") // Elimina saltos de línea
+
+        // Divide la cadena en requisitos mínimos y recomendados
+        val requirementParts = cleanString.split("Recommended:")
+
+        // Obtén los requisitos mínimos si están presentes
+        val minimumRequirement = requirementParts.getOrNull(0)?.let { minimum ->
+            val minimumParts = minimum.split("OS:", "Processor:", "Memory:", "Graphics:", "DirectX:", "Sound Card:", "Network:", "Storage:")
+            if (minimumParts.size >= 9) {
+                GameDetail.PcRequirement(
+                    os = minimumParts.getOrElse(1) { "" }.trim(),
+                    processor = minimumParts.getOrElse(2) { "" }.trim(),
+                    memory = minimumParts.getOrElse(3) { "" }.trim(),
+                    graphics = minimumParts.getOrElse(4) { "" }.trim(),
+                    directx = minimumParts.getOrElse(5) { "" }.trim(),
+                    soundcard = minimumParts.getOrElse(6) { "" }.trim(),
+                    network = minimumParts.getOrElse(7) { "" }.trim(),
+                    storage = minimumParts.getOrElse(8) { "" }.trim(),
+                )
+            } else {
+                null
+            }
+        }
+
+        minimumRequirement?.let {
+            Log.d(tag, "OS: ${it.os}")
+        }
+
+        // Obtén los requisitos recomendados si están presentes
+        val recommendedRequirement = if (requirementParts.size > 1) {
+            requirementParts.getOrNull(1)?.let { recommended ->
+                val recommendedParts = recommended.split("OS:", "Processor:", "Memory:", "Graphics:", "DirectX:", "Sound Card:", "Network:", "Storage:")
+                if (recommendedParts.size >= 7) {
+                    GameDetail.PcRequirement(
+                        os = recommendedParts.getOrElse(1) { "" }.trim(),
+                        processor = recommendedParts.getOrElse(2) { "" }.trim(),
+                        memory = recommendedParts.getOrElse(3) { "" }.trim(),
+                        graphics = recommendedParts.getOrElse(4) { "" }.trim(),
+                        directx = recommendedParts.getOrElse(5) { "" }.trim(),
+                        soundcard = recommendedParts.getOrElse(6) { "" }.trim(),
+                        network = recommendedParts.getOrElse(7) { "" }.trim(),
+                        storage = recommendedParts.getOrElse(8) { "" }.trim()
+                    )
+                } else {
+                    null
+                }
+            }
+        } else {
+            // Si no hay requisitos recomendados, utiliza los mismos requisitos mínimos
+            minimumRequirement
+        }
+
+        if (minimumRequirement != null && recommendedRequirement != null) {
+            // Crea el objeto PcRequirements con los requisitos mínimos y recomendados
+            return GameDetail.PcRequirements(minimumRequirement, recommendedRequirement)
+        }
+
+        return null
     }
 
     fun onChangeThemeButtonClick(view: View) {
--- app/src/main/java/com/ar/sebastiangomez/steam/GameDetail.kt
+++ app/src/main/java/com/ar/sebastiangomez/steam/GameDetail.kt
@@ -15,7 +15,6 @@ class GameDetail {
         val detailed_description: String,
         val about_the_game: String,
         val short_description: String,
-        val fullgame: FullGame?,
         val header_image: String,
         val capsule_image: String,
         val capsule_imagev5: String,
@@ -26,78 +25,23 @@ class GameDetail {
         val developers: List<String>,
         val publishers: List<String>,
         val package_groups: List<Any>, // Puedes cambiar el tipo si es un objeto con propiedades específicas
-        val platforms: Platforms,
-        val categories: List<Category>,
-        val achievements: Achievements?,
-        val release_date: ReleaseDate,
-        val support_info: SupportInfo,
         val background: String,
         val background_raw: String,
-        val content_descriptors: ContentDescriptors,
-        val ratings: Ratings
     )
 
-    data class FullGame(
-        val appid: String,
-        val name: String
+    data class PcRequirements(
+        val minimum: PcRequirement,
+        val recommended: PcRequirement
     )
 
-    data class Platforms(
-        val windows: Boolean,
-        val mac: Boolean,
-        val linux: Boolean
-    )
-
-    data class Category(
-        val id: Int,
-        val description: String
-    )
-
-    data class Achievements(
-        val total: Int,
-        val highlighted: List<Highlighted>
-    )
-
-    data class Highlighted(
-        val name: String,
-        val path: String
-    )
-
-    data class ReleaseDate(
-        val coming_soon: Boolean,
-        val date: String
-    )
-
-    data class SupportInfo(
-        val url: String,
-        val email: String
-    )
-
-    data class ContentDescriptors(
-        val ids: List<Any>, // Puedes cambiar el tipo si los descriptores tienen una estructura específica
-        val notes: Any? // Puedes cambiar el tipo si los descriptores tienen una estructura específica
-    )
-
-    data class Ratings(
-        val dejus: Dejus,
-        val steam_germany: SteamGermany
-    )
-
-    data class Dejus(
-        val rating_generated: String,
-        val rating: String,
-        val required_age: String,
-        val banned: String,
-        val use_age_gate: String,
-        val descriptors: String
-    )
-
-    data class SteamGermany(
-        val rating_generated: String,
-        val rating: String,
-        val required_age: String,
-        val banned: String,
-        val use_age_gate: String,
-        val descriptors: String
+    data class PcRequirement(
+        val os: String,
+        val processor: String,
+        val memory: String,
+        val graphics: String,
+        val directx: String,
+        val soundcard: String,
+        val network: String,
+        val storage: String
     )
 }
