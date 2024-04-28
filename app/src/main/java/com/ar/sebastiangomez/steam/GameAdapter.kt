f4af7cdcc98bf7f0153e9b2946e9641a73bcbba3
Sebastian Gomez <SSGOMEZ@trabajo.gob.ar>
2024-04-27 23:54:45 -0300

    Cambio en el renderizado de la lista.

--- app/src/main/java/com/ar/sebastiangomez/steam/GameAdapter.kt
+++ app/src/main/java/com/ar/sebastiangomez/steam/GameAdapter.kt
@@ -6,12 +6,14 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class GameAdapter(private val gamesList: List<Game>, private val onItemClick: (position: Int, gameId: String) -> Unit) : RecyclerView.Adapter<GameAdapter.GameViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GameViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(android.R.layout.simple_list_item_1, parent, false)
+        //val view = LayoutInflater.from(parent.context).inflate(android.R.layout.simple_list_item_1, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_custom_item, parent, false)
         return GameViewHolder(view)
     }
 
@@ -33,6 +35,12 @@ class GameAdapter(private val gamesList: List<Game>, private val onItemClick: (p
             intent.putExtra("ID", selectID)
             holder.itemView.context.startActivity(intent)
         }
+
+        holder.imageButton.setOnClickListener {
+            // Aquí se ejecuta la acción cuando se hace clic en el ImageButton
+            // Puedes llamar a una función o realizar cualquier acción necesaria
+            Log.d("ImageButton Clicked", "Position: $position, Game ID: ${game.id}")
+        }
     }
 
     override fun getItemCount(): Int {
@@ -40,7 +48,9 @@ class GameAdapter(private val gamesList: List<Game>, private val onItemClick: (p
     }
 
     class GameViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val textView: TextView = itemView.findViewById(android.R.id.text1)
+        //val textView: TextView = itemView.findViewById(android.R.id.text1)
+        val textView: TextView = itemView.findViewById(R.id.textList)
+        val imageButton: ImageButton = itemView.findViewById(R.id.imageButtonList)
         fun bind(game: Game) {
             textView.text = game.name
         }
