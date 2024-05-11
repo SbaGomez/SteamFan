7c2d9f1ad5d78c8da4f17f4471f2319dfaf5d974
Sebastian Gomez <SSGOMEZ@trabajo.gob.ar>
2024-05-11 01:52:32 -0300

    Cambio de nombre de tag y uso de SearchHelper.

--- app/src/main/java/com/ar/sebastiangomez/steam/BookmarkAdapter.kt
+++ app/src/main/java/com/ar/sebastiangomez/steam/BookmarkAdapter.kt
@@ -2,7 +2,6 @@ package com.ar.sebastiangomez.steam
 
 import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -10,18 +9,19 @@ import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
+import com.ar.sebastiangomez.steam.utils.GamesFromCache
 
 class BookmarkAdapter(private val context: Context, private val gamesList: List<Game>, private val onItemClick: (position: Int, gameId: String) -> Unit) : RecyclerView.Adapter<BookmarkAdapter.GameViewHolder>() {
 
-    private val tag = "LOG-LIST"
+    private lateinit var gamesFromCache: GamesFromCache
+    private val tag = "LOG-BOOKMARK-LIST"
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GameViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_detail_item, parent, false)
         return GameViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: GameViewHolder, position: Int) {
+        gamesFromCache = GamesFromCache()
         val game = gamesList[position]
         holder.bind(game)
 
@@ -37,7 +37,7 @@ class BookmarkAdapter(private val context: Context, private val gamesList: List<
 
         holder.imageButton.setOnClickListener {
             // Obtener la lista de juegos en caché
-            val cachedGames = getGamesFromCache(context)
+            val cachedGames = gamesFromCache.getGamesFromCache(context)
 
             // Encontrar el índice del juego a eliminar en la lista
             val indexToRemove = cachedGames.indexOfFirst { it.id == game.id }
@@ -47,33 +47,18 @@ class BookmarkAdapter(private val context: Context, private val gamesList: List<
                 cachedGames.removeAt(indexToRemove)
 
                 // Guardar la lista actualizada en la caché
-                saveGamesToCache(context, cachedGames)
+                gamesFromCache.saveGamesToCache(context, cachedGames)
 
                 // Log de la acción y cualquier otra acción necesaria
                 Log.d(tag, "Removed game from cache - ID: ${game.id}, Name: ${game.name}")
 
                 // Iniciar la actividad de marcadores (o cualquier otra acción necesaria)
-                val intent = Intent(holder.itemView.context, BookmarkActivity::class.java)
-                holder.itemView.context.startActivity(intent)
+                (holder.itemView.context as BookmarkActivity).recreate()
             } else {
                 // El juego no se encontró en la lista en caché
                 Log.d(tag, "Game not found in cache - ID: ${game.id}, Name: ${game.name}")
             }
         }
-    }
-
-    private fun getGamesFromCache(context: Context): MutableList<Game> {
-        val sharedPreferences: SharedPreferences = context.getSharedPreferences("bookmark_data", Context.MODE_PRIVATE)
-        val json = sharedPreferences.getString("games", "[]")
-        val type = object : TypeToken<MutableList<Game>>() {}.type
-        return Gson().fromJson(json, type)
-    }
-    private fun saveGamesToCache(context: Context, games: MutableList<Game>) {
-        val sharedPreferences: SharedPreferences = context.getSharedPreferences("bookmark_data", Context.MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
-        val json = Gson().toJson(games)
-        editor.putString("games", json)
-        editor.apply()
     }
 
     override fun getItemCount(): Int {
