7159e383bd77fa6f9c6b3edef560ea14370367c6
Sebastian Gomez <SSGOMEZ@trabajo.gob.ar>
2024-05-19 00:26:14 -0300

    new class GameCached

--- app/src/main/java/com/ar/sebastiangomez/steam/utils/GamesCache.kt
+++ app/src/main/java/com/ar/sebastiangomez/steam/utils/GamesCache.kt
@@ -2,21 +2,21 @@ package com.ar.sebastiangomez.steam.utils
 
 import android.content.Context
 import android.content.SharedPreferences
-import com.ar.sebastiangomez.steam.model.Game
+import com.ar.sebastiangomez.steam.model.GameCached
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
 class GamesCache {
     // Función para obtener la lista de IDs almacenados en caché
-    fun getGamesFromCache(context: Context): MutableList<Game> {
+    fun getGamesFromCache(context: Context): MutableList<GameCached> {
         val sharedPreferences: SharedPreferences = context.getSharedPreferences("bookmark_data", Context.MODE_PRIVATE)
         val json = sharedPreferences.getString("games", "[]")
-        val type = object : TypeToken<MutableList<Game>>() {}.type
+        val type = object : TypeToken<MutableList<GameCached>>() {}.type
         return Gson().fromJson(json, type)
     }
 
     // Función para guardar la lista de IDs en caché
-    fun saveGamesToCache(context: Context, games: MutableList<Game>) {
+    fun saveGamesToCache(context: Context, games: MutableList<GameCached>) {
         val sharedPreferences: SharedPreferences = context.getSharedPreferences("bookmark_data", Context.MODE_PRIVATE)
         val editor = sharedPreferences.edit()
         val json = Gson().toJson(games)
@@ -25,7 +25,7 @@ class GamesCache {
     }
 
     // Función para agregar un ID a la lista en caché
-    fun addGameToCache(context: Context, game: Game) {
+    fun addGameToCache(context: Context, game: GameCached) {
         val games = getGamesFromCache(context)
         if (!games.any { it.id == game.id }) {
             games.add(game)
