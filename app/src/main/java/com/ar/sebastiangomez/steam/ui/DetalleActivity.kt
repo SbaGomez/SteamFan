847b16599e941360560188f95d9fa4d3d0ec2b3d
Sebastian Gomez <SSGOMEZ@trabajo.gob.ar>
2024-05-25 20:12:21 -0300

    Mejoras de codigo.

--- app/src/main/java/com/ar/sebastiangomez/steam/ui/DetalleActivity.kt
+++ app/src/main/java/com/ar/sebastiangomez/steam/ui/DetalleActivity.kt
@@ -27,14 +27,14 @@ import com.ar.sebastiangomez.steam.data.DolarRepository
 import com.ar.sebastiangomez.steam.data.GamesRepository
 import com.ar.sebastiangomez.steam.model.GameCached
 import com.ar.sebastiangomez.steam.model.GameDetail
-import com.ar.sebastiangomez.steam.model.PcRequirement
-import com.ar.sebastiangomez.steam.model.PcRequirements
 import com.ar.sebastiangomez.steam.utils.GamesCache
 import com.ar.sebastiangomez.steam.utils.ThemeHelper
+import com.ar.sebastiangomez.steam.utils.Utils
 import com.bumptech.glide.Glide
 import kotlinx.coroutines.launch
 
 class DetalleActivity : AppCompatActivity() {
+    private lateinit var utils: Utils
     private lateinit var gamesCache: GamesCache
     private lateinit var progressBar : ProgressBar
     private lateinit var titleTxt : TextView
@@ -94,6 +94,7 @@ class DetalleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         gamesCache = GamesCache()
+        utils = Utils()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_detalle)
@@ -193,7 +194,7 @@ class DetalleActivity : AppCompatActivity() {
 
     @SuppressLint("SetTextI18n", "DefaultLocale")
     private suspend fun setData(gameDetail : GameDetail){
-        val pcRequirements = parsePcRequirements(gameDetail.pc_requirements.toString())
+        val pcRequirements = utils.parsePcRequirements(gameDetail.pc_requirements.toString())
         val cachedGames = gamesCache.getGamesFromCache(this@DetalleActivity)
         var indexID = cachedGames.indexOfFirst { it.id == gameDetail.steam_appid.toString() }
 
@@ -405,87 +406,6 @@ class DetalleActivity : AppCompatActivity() {
         else{
             layoutDetalle.removeView(cardRec)
         }
-    }
-
-    private fun parsePcRequirements(pcRequirementsString: String): PcRequirements? {
-        // Elimina las etiquetas HTML y caracteres innecesarios
-        val cleanString = pcRequirementsString
-            .replace("<strong>", " ")
-            .replace("<br><ul class=\"bb_ul\"><li>Requires a 64-bit processor and operating system<br></li><li>", " ")
-            .replace("OS *:", "OS:")
-            .replace("minimum=", "")
-            .replace("recommended=", "")
-            .replace(Regex("<.*?>"), "") // Elimina etiquetas HTML
-            .replace(",  Recommended:Requires a 64-bit processor and operating system", " Recommended: OS: Requires a 64-bit processor and operating system")
-            .replace(Regex("Sound Card:"), "Sound:") // Elimina etiquetas HTML
-            .replace(Regex(" space,"), "") // Elimina etiquetas HTML
-            .replace("\n", "") // Elimina saltos de línea
-
-        // Divide la cadena en requisitos mínimos y recomendados
-        val requirementParts = cleanString.split("Recommended:")
-
-        // Función auxiliar para extraer un requisito específico de acuerdo a su etiqueta
-        fun extractRequirement(requirementString: String, label: String): String {
-            val regex =
-                Regex("$label\\s*:?\\s*((?:(?!$label\\s*:?)[^:])*)", RegexOption.IGNORE_CASE)
-            val matchResult = regex.find(requirementString)
-            val content = matchResult?.groupValues?.getOrNull(1)?.trim() ?: ""
-
-            // Eliminar la última palabra del contenido
-            val words = content.split(" ")
-
-            return if (words.size > 1) words.dropLast(1).joinToString(" ") else content
-        }
-
-        // Función auxiliar para crear un objeto PcRequirement a partir de una cadena de requisito
-        fun createPcRequirement(requirementString: String): PcRequirement? {
-            val os = extractRequirement(requirementString, "OS:")
-            val processor = extractRequirement(requirementString, "Processor:")
-            val memory = extractRequirement(requirementString, "Memory:")
-            val graphics = extractRequirement(requirementString, "Graphics:")
-            val directx = extractRequirement(requirementString, "DirectX:")
-            val soundcard = extractRequirement(requirementString, "Sound:")
-            val network = extractRequirement(requirementString, "Network:")
-            val storage = extractRequirement(requirementString, "Storage:")
-
-            return if (os.isNotEmpty() || processor.isNotEmpty() || memory.isNotEmpty() || graphics.isNotEmpty() ||
-                directx.isNotEmpty() || soundcard.isNotEmpty() || network.isNotEmpty() || storage.isNotEmpty()) {
-                PcRequirement(
-                    os = os,
-                    processor = processor,
-                    memory = memory,
-                    graphics = graphics,
-                    directx = directx,
-                    soundcard = soundcard,
-                    network = network,
-                    storage = storage
-                )
-            } else {
-                null
-            }
-        }
-
-        // Obtén los requisitos mínimos si están presentes
-        val minimumRequirement = requirementParts.getOrNull(0)?.let { minimum ->
-            createPcRequirement(minimum)
-        }
-
-        // Obtén los requisitos recomendados si están presentes
-        val recommendedRequirement = if (requirementParts.size > 1)
-        {
-            requirementParts.getOrNull(1)?.let { recommended ->
-                createPcRequirement(recommended)
-            }
-        } else {
-            // Si no hay requisitos recomendados, utiliza los mismos requisitos mínimos
-            minimumRequirement
-        }
-
-        if (minimumRequirement != null || recommendedRequirement != null) {
-            // Crea el objeto PcRequirements con los requisitos mínimos y recomendados
-            return PcRequirements(minimumRequirement, recommendedRequirement)
-        }
-        return null
     }
 
     @Suppress("UNUSED_PARAMETER")
--- app/src/main/java/com/ar/sebastiangomez/steam/utils/Utils.kt
+++ app/src/main/java/com/ar/sebastiangomez/steam/utils/Utils.kt
@@ -3,6 +3,8 @@ package com.ar.sebastiangomez.steam.utils
 import android.app.Activity
 import android.content.Context
 import android.view.inputmethod.InputMethodManager
+import com.ar.sebastiangomez.steam.model.PcRequirement
+import com.ar.sebastiangomez.steam.model.PcRequirements
 
 class Utils {
     fun hideKeyboard(activity: Activity) {
@@ -10,5 +12,86 @@ class Utils {
         activity.currentFocus?.let {
             inputMethodManager.hideSoftInputFromWindow(it.windowToken, 0)
         }
+    }
+
+    fun parsePcRequirements(pcRequirementsString: String): PcRequirements? {
+        // Elimina las etiquetas HTML y caracteres innecesarios
+        val cleanString = pcRequirementsString
+            .replace("<strong>", " ")
+            .replace("<br><ul class=\"bb_ul\"><li>Requires a 64-bit processor and operating system<br></li><li>", " ")
+            .replace("OS *:", "OS:")
+            .replace("minimum=", "")
+            .replace("recommended=", "")
+            .replace(Regex("<.*?>"), "") // Elimina etiquetas HTML
+            .replace(",  Recommended:Requires a 64-bit processor and operating system", " Recommended: OS: Requires a 64-bit processor and operating system")
+            .replace(Regex("Sound Card:"), "Sound:") // Elimina etiquetas HTML
+            .replace(Regex(" space,"), "") // Elimina etiquetas HTML
+            .replace("\n", "") // Elimina saltos de línea
+
+        // Divide la cadena en requisitos mínimos y recomendados
+        val requirementParts = cleanString.split("Recommended:")
+
+        // Función auxiliar para extraer un requisito específico de acuerdo a su etiqueta
+        fun extractRequirement(requirementString: String, label: String): String {
+            val regex =
+                Regex("$label\\s*:?\\s*((?:(?!$label\\s*:?)[^:])*)", RegexOption.IGNORE_CASE)
+            val matchResult = regex.find(requirementString)
+            val content = matchResult?.groupValues?.getOrNull(1)?.trim() ?: ""
+
+            // Eliminar la última palabra del contenido
+            val words = content.split(" ")
+
+            return if (words.size > 1) words.dropLast(1).joinToString(" ") else content
+        }
+
+        // Función auxiliar para crear un objeto PcRequirement a partir de una cadena de requisito
+        fun createPcRequirement(requirementString: String): PcRequirement? {
+            val os = extractRequirement(requirementString, "OS:")
+            val processor = extractRequirement(requirementString, "Processor:")
+            val memory = extractRequirement(requirementString, "Memory:")
+            val graphics = extractRequirement(requirementString, "Graphics:")
+            val directx = extractRequirement(requirementString, "DirectX:")
+            val soundcard = extractRequirement(requirementString, "Sound:")
+            val network = extractRequirement(requirementString, "Network:")
+            val storage = extractRequirement(requirementString, "Storage:")
+
+            return if (os.isNotEmpty() || processor.isNotEmpty() || memory.isNotEmpty() || graphics.isNotEmpty() ||
+                directx.isNotEmpty() || soundcard.isNotEmpty() || network.isNotEmpty() || storage.isNotEmpty()) {
+                PcRequirement(
+                    os = os,
+                    processor = processor,
+                    memory = memory,
+                    graphics = graphics,
+                    directx = directx,
+                    soundcard = soundcard,
+                    network = network,
+                    storage = storage
+                )
+            } else {
+                null
+            }
+        }
+
+        // Obtén los requisitos mínimos si están presentes
+        val minimumRequirement = requirementParts.getOrNull(0)?.let { minimum ->
+            createPcRequirement(minimum)
+        }
+
+        // Obtén los requisitos recomendados si están presentes
+        val recommendedRequirement = if (requirementParts.size > 1)
+        {
+            requirementParts.getOrNull(1)?.let { recommended ->
+                createPcRequirement(recommended)
+            }
+        } else {
+            // Si no hay requisitos recomendados, utiliza los mismos requisitos mínimos
+            minimumRequirement
+        }
+
+        if (minimumRequirement != null || recommendedRequirement != null) {
+            // Crea el objeto PcRequirements con los requisitos mínimos y recomendados
+            return PcRequirements(minimumRequirement, recommendedRequirement)
+        }
+        return null
     }
 }
