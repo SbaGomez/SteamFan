e25ea55e60b9b4844a7dfd3584784ef0caa73e21
Sebastian Gomez <SSGOMEZ@trabajo.gob.ar>
2024-06-20 16:34:22 -0300

    Funcionamiento cambio de idioma.

--- app/src/main/java/com/ar/sebastiangomez/steam/ui/ConfigActivity.kt
+++ app/src/main/java/com/ar/sebastiangomez/steam/ui/ConfigActivity.kt
@@ -1,17 +1,28 @@
 package com.ar.sebastiangomez.steam.ui
-
+import android.content.Context
+import android.content.SharedPreferences
+import android.content.res.Configuration
 import android.os.Bundle
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.ar.sebastiangomez.steam.R
+import java.util.*
 
 class ConfigActivity : AppCompatActivity() {
+
+    companion object {
+        const val PREFS_NAME = "MyPrefsFile"
+        const val LANGUAGE_KEY = "Language"
+        const val DEFAULT_LANGUAGE = "es" // Establece "en" como el valor predeterminado
+    }
+
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,15 +32,52 @@ class ConfigActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        sharedPreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
 
-        val items = listOf("Español", "Ingles")
-        val autoComplete: AutoCompleteTextView = findViewById(R.id.auto_complete)
-        val adapter = ArrayAdapter(this, R.layout.list_item, items)
-        autoComplete.setAdapter(adapter)
-        autoComplete.onItemClickListener =
-            AdapterView.OnItemClickListener { adapterView, view, i, l ->
-                val itemSelected = adapterView.getItemAtPosition(i)
-                Toast.makeText(this, "Idioma seleccionado: $itemSelected", Toast.LENGTH_SHORT).show()
+        val radioGroupLanguages = findViewById<RadioGroup>(R.id.radioGroupLanguages)
+
+        // Obtener el idioma guardado en las SharedPreferences
+        val currentLanguage = sharedPreferences.getString(LANGUAGE_KEY, DEFAULT_LANGUAGE)
+
+        // Marcar el RadioButton correspondiente al idioma guardado
+        when (currentLanguage) {
+            "en" -> radioGroupLanguages.check(R.id.radioButtonEnglish)
+            "es" -> radioGroupLanguages.check(R.id.radioButtonSpanish)
+            "pt" -> radioGroupLanguages.check(R.id.radioButtonPortuguese)
+        }
+
+        radioGroupLanguages.setOnCheckedChangeListener { group, checkedId ->
+            val radioButton = findViewById<RadioButton>(checkedId)
+            val selectedLanguage = radioButton.text.toString()
+
+            // Obtener el idioma actual de la configuración
+            val currentLocale = Locale.getDefault().language
+
+            if (currentLocale != selectedLanguage) {
+                when (selectedLanguage) {
+                    getString(R.string.english) -> setLocale("en")
+                    getString(R.string.spanish) -> setLocale("es")
+                    getString(R.string.portuguese) -> setLocale("pt")
+                }
+
+                // Guardar el idioma seleccionado en las SharedPreferences
+                sharedPreferences.edit().putString(LANGUAGE_KEY, selectedLanguage).apply()
+
+                Toast.makeText(this, "Selected: $selectedLanguage", Toast.LENGTH_SHORT).show()
             }
+        }
+    }
+
+    private fun setLocale(languageCode: String) {
+        val locale = Locale(languageCode)
+        Locale.setDefault(locale)
+        val config = Configuration()
+        config.setLocale(locale)
+        baseContext.resources.updateConfiguration(config, baseContext.resources.displayMetrics)
+
+        // Reiniciar la actividad para aplicar el nuevo idioma
+        val intent = intent
+        finish()
+        startActivity(intent)
     }
 }
